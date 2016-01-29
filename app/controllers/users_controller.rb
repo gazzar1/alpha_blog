542ca8665cf8d49aaa56dca5fad9060e15e7ddd9
gazzar <mohamed.algazzar@gmail.com>
2016-01-29 15:27:00 +0000

    Add admin functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:edit, :update, :show]
-  before_action :require_same_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
+  before_action :require_admin, only: [:destroy]
   def index
     @users = User.paginate(page: params[:page], per_page: 3)
   end
@@ -34,7 +35,12 @@ class UsersController < ApplicationController
   def show
      @user_articles = @user.articles.paginate(page: params[:page], per_page: 3)
   end
-
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:danger] = "User and all articles created by user has been deleted"
+    redirect_to users_path
+  end
 
   private
   def user_params
@@ -44,8 +50,14 @@ class UsersController < ApplicationController
    @user = User.find(params[:id])
   end
   def require_same_user
-    if !logged_in? || current_user != @user
+    if current_user != @user and !current_user.admin?
       flash[:danger] = "You can only edit your own user account"
+      redirect_to root_path
+    end
+  end
+  def require_admin
+    if logged_in? and !current_user.admin?
+      flash[:danger] = "Only admin users can perform that action"
       redirect_to root_path
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160123103232) do
+ActiveRecord::Schema.define(version: 20160127173812) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20160123103232) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
+    t.boolean  "admin",           default: false
   end
 
 end
