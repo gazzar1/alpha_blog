efeefe0bb91a7a044ea968a2fe55ee9bd5e82cc5
gazzar <mohamed.algazzar@gmail.com>
2016-02-04 14:04:59 +0000

    update navigation bar with categories

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -4,6 +4,7 @@ class CategoriesControllerTest < ActionController::TestCase
   def setup
 
   @category = Category.create(name: "sports")
+    @user = User.create(username: "fawzy", email: "fawzy@gmail.com", password: "123", admin: true)
 end
   test "should get catgory index" do
     get :index
@@ -11,6 +12,7 @@ end
   end
 
   test "should get new" do
+    session[:user_id] = @user.id
     get :new
     assert_response :success
   end
@@ -21,5 +23,13 @@ end
     get(:show, {'id' => @category.id})
     assert_response :success
   end
+
+  test "should redirect create when admin not logged in" do
+    assert_no_difference 'Category.count' do
+      post :create, category: { name: "sporte"}
+    end
+    assert_redirected_to categories_path
+  end
+
 
 end
